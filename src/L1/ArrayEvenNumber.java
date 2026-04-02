8330c22c6d248703638982350623ca6830c3951d
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-02 22:55:28 +0300

    Initial commit

--- src/L1/ArrayEvenNumber.java
+++ src/L1/ArrayEvenNumber.java
@@ -3,6 +3,10 @@ package L1;
 public class ArrayEvenNumber {
 
     public static void main(String[] args) {
-
+        int[] numbers = {5, 2, 6, 3, 4};
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0)
+                System.out.println(numbers[i]);
+        }
     }
 }
