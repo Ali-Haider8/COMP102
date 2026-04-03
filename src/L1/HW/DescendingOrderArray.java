746f9a7830c23df06150cd512cfc99e04b8684b0
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-03 09:50:20 +0300

    Initial commit

---
+++ src/L1/HW/DescendingOrderArray.java
@@ -0,0 +1,43 @@
+package L1.HW;
+
+import java.util.Scanner;
+
+@SuppressWarnings("DuplicatedCode")
+public class DescendingOrderArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        System.out.print("Enter 5 numbers: ");
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        System.out.print("Array numbers are: ");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+        System.out.println();
+        System.out.print("Descending Order: ");
+
+        int swap;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] < numbers[j]) {
+                    swap = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = swap;
+                }
+            }
+        }
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+    }
+}
