2e10bc3a17866ee4a355baa07cfa6264e3773e2c
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-03 10:45:46 +0300

    Initial commit

---
+++ src/L1/HW/FindAStringInArray.java
@@ -0,0 +1,43 @@
+package L1.HW;
+
+import java.util.Scanner;
+
+public class FindAStringInArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String[] names = new String[5];
+
+        System.out.println("Enter 5 names (Press enter after each name): ");
+
+        for (int i = 0; i < names.length; i++) {
+            names[i] = sc.nextLine();
+        }
+
+        System.out.println();
+        System.out.println("The elements of the array are: ");
+
+        for (String name : names) {
+            System.out.print(name + " ");
+        }
+
+        System.out.println();
+        System.out.print("Find a name in array: ");
+        String targetName = sc.nextLine();
+
+        int flag = 0;
+        int i;
+        for (i = 0; i < names.length; i++) {
+            if (names[i].equals(targetName)) {
+                flag = i;
+            }
+        }
+
+        if (flag != 0) {
+            System.out.println("The name is found in position " + (flag + 1));
+        } else {
+            System.out.println("The name wasn't founded in the array");
+        }
+    }
+}
