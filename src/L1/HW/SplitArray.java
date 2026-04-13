661602eb2d3faa03f26382907cd5cab78936443e
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-13 08:58:25 +0300

    Initial commit

--- src/L1/HW/SplitArray.java
+++ src/L1/HW/SplitArray.java
@@ -1,32 +1,36 @@
 package L1.HW;
+
 //  4. Create array (1, 2, 3, 4, 5, 6) then Split an Array into Two Halves:
+@SuppressWarnings("ManualArrayCopy")
 public class SplitArray {
 
     static void main(String[] args) {
 
-        int[] numbers = {1, 2, 3, 4, 5, 6};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        int[] firstHalf = new int[3];
-        int[] secondHalf = new int[3];
+        int[] A = new int[numbers.length / 2];
+        int[] B = new int[numbers.length / 2];
 
         System.out.print("Original array: ");
         for (int x : numbers) {
             System.out.print(x + " ");
         }
 
-        System.arraycopy(numbers, 0, firstHalf, 0, firstHalf.length);
+        for (int i = 0; i < (numbers.length / 2); i++) {
+            A[i] = numbers[i];
+        }
 
-        System.arraycopy(numbers, 3, secondHalf, 0, secondHalf.length);
+        for (int i = 5; i < numbers.length; i++) {
+            B[i - 5] = numbers[i];
+        }
 
-        System.out.println();
-        System.out.print("First array: ");
-        for (int x : firstHalf) {
+        System.out.print("\nArray A: ");
+        for (int x : A) {
             System.out.print(x + " ");
         }
 
-        System.out.println();
-        System.out.print("Second Half: ");
-        for (int x : secondHalf) {
+        System.out.print("\nArray B: ");
+        for (int x : B) {
             System.out.print(x + " ");
         }
     }
