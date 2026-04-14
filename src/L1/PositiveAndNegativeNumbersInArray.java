a130b8ef88065aa78841277361c009f1c2c5aac0
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-14 12:13:24 +0300

    Initial commit

---
+++ src/L1/PositiveAndNegativeNumbersInArray.java
@@ -0,0 +1,27 @@
+package L1;
+
+class PositiveAndNegativeNumbersInArray {
+
+    static void main() {
+
+        int[] arr = {-3, -2, -1, 0, 1, 2, 3};
+        System.out.print("Array: ");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+
+        System.out.print("\nPositive numbers in array: ");
+        for (int x : arr) {
+            if (x > 0) {
+                System.out.print(x + " ");
+            }
+        }
+
+        System.out.print("\nNegative numbers in array: ");
+        for (int x : arr) {
+            if (x < 0) {
+                System.out.print(x + " ");
+            }
+        }
+    }
+}
