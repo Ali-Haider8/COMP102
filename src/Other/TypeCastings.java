e31d800998988d11cd22e113574f48ffc27a76fb
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-16 07:09:36 +0300

    Initial commit

--- src/Other/TypeCastings.java
+++ src/Other/TypeCastings.java
@@ -3,12 +3,51 @@ package Other;
 import java.util.Scanner;
 
 public class TypeCastings {
-    static void main(String[] args) {
+    static void main() {
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter char value from ASCII Table: ");
-        char a = input.next().charAt(0);
-        int z = a;
-        System.out.println(z);
+
+
+        System.out.println("Select a type to casting");
+        System.out.println("1) from char to int");
+        System.out.println("2) from int to char");
+        System.out.println("3) from double to int");
+        System.out.println("4) from int to double");
+        System.out.print("Number: ");
+        int choice = input.nextInt();
+
+        switch (choice) {
+            case 1:
+                char a;
+                int z;
+                System.out.print("Enter a char value: ");
+                a = input.next().charAt(0);
+                z = a;
+                System.out.println("Int value for char " + a + " is " + z);
+                break;
+            case 2:
+                System.out.print("Enter an int value: ");
+                int x = input.nextInt();
+                char b = (char) x;
+                System.out.println("Char value for int " + x + " is " + b);
+                break;
+            case 3:
+                double d;
+                int y;
+                System.out.print("Enter a double value: ");
+                d = input.nextDouble();
+                y = (int) d;
+                System.out.println("Int value for double " + d + " is " + y);
+                break;
+            case 4:
+                int k;
+                double f;
+                System.out.print("Enter an int value: ");
+                k = input.nextInt();
+                f = k;
+                System.out.println("Double value for int " + k + " is " + f);
+            default:
+                System.out.println("Invalid choice");
+        }
     }
 }
