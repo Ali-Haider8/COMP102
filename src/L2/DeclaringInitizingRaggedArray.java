43b502378e8ea4b63cc7b4717892474356b6a641
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-17 15:46:36 +0300

    Initial commit

---
+++ src/L2/DeclaringInitizingRaggedArray.java
@@ -0,0 +1,21 @@
+package L2;
+
+class DeclaringInitizingRaggedArray {
+
+    static void main() {
+
+        char[][] animals = {
+                {'M', 'O', 'N', 'K', 'E', 'Y'},
+                {'C', 'A', 'T'},
+                {'B', 'I', 'R', 'D'}
+        };
+
+        for (int row = 0; row < animals.length; row++) {
+            for (int col = 0; col < animals[row].length; col++) {
+                System.out.print(animals[row][col]);
+            }
+            System.out.println();
+        }
+
+    }
+}
