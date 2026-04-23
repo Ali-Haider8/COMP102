b48580dd860db55d6c2a8a83821ba2c331a1dc67
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-23 23:56:29 +0300

    solve a homework

---
+++ src/L2/HW/Print2DArrayInMatrixForm.java
@@ -0,0 +1,32 @@
+package L2.HW;
+
+import java.util.Scanner;
+
+public class Print2DArrayInMatrixForm {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row: ");
+        int row = sc.nextInt();
+
+        System.out.print("Enter column: ");
+        int column = sc.nextInt();
+
+        int[][] arr = new int[row][column];
+        System.out.print("Enter " + (row * column) + " elements: ");
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Array in matrix form");
+        for (int[] rows : arr) {
+            for (int element : rows) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
