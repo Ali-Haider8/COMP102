91e39a199f2df3c111391c4c940c39f39b3f30ac
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-26 01:44:50 +0300

    solve a homework

--- src/L1/HW/SecondLargestElementWithoutSorting.java
+++ src/L1/HW/SecondLargestElementWithoutSorting.java
@@ -1,22 +1,25 @@
 package L1.HW;
-// 6. Create array (10, 20, 4, 45, 99) then , Find Second Largest Element
-//Without Sorting:
+
+// 6. Create array (10, 20, 4, 45, 99) then , Find Second Largest Element Without Sorting:
 public class SecondLargestElementWithoutSorting {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
-        int[] numbers = {10, 20, 4, 45, 99};
+        int[] arr = {10, 20, 4, 45, 99};
 
-        int largestNumber = numbers[0];
-        int secondLargestNumber = numbers[0];
+        int largestNumber = arr[0];
+        int secondLargestNumber = arr[0];
 
-        for (int num : numbers) {
-            if (num > largestNumber) {
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > largestNumber) {
                 secondLargestNumber = largestNumber;
-                largestNumber = num;
-            } else if (num > secondLargestNumber && num != largestNumber) {
-                secondLargestNumber = num;
+                largestNumber = arr[i];
+
+            } else if (arr[i] > secondLargestNumber && arr[i] != largestNumber) {
+                secondLargestNumber = i;
             }
+
         }
 
         System.out.println("Second Largest Number is: " + secondLargestNumber);
