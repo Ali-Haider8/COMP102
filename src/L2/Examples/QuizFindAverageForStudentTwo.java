e51d8edc500ab057edca81e963e0af68d3870452
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-26 22:55:11 +0300

    solve a homework

--- src/L2/Examples/QuizFindAverageForStudentTwo.java
+++ src/L2/Examples/QuizFindAverageForStudentTwo.java
@@ -3,9 +3,9 @@ package L2.Examples;
 public class QuizFindAverageForStudentTwo {
 
     public static void main(String[] args) {
-        int[][] arr = {{30, 40, 59, 49}, {54, 45, 76, 45}, {45, 23, 65, 23}};
+        int[][] arr = {{30, 40, 59, 49}, {50, 50, 100, 50}, {45, 23, 65, 23}};
         int sum = 0;
-        for (int i = 0; i < arr.length; i++)
+        for (int i = 0; i < arr.length + 1; i++)
             sum += arr[1][i];
         System.out.println(sum / 4);
     }
