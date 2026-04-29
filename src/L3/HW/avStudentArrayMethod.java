53932eda363ebf1ba212548f965e7098a79f88e9
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-04-29 10:37:31 +0300

    solve a quiz

---
+++ src/L3/HW/avStudentArrayMethod.java
@@ -0,0 +1,28 @@
+package L3.HW;
+
+/*
+Write java program to create
+avstudent(st[],d[][]) to find the sum and
+av for the following arryas of students
+Ali 10, 10,20
+Moh 20,100,50
+Hassan 100,1 00,100
+*/
+public class avStudentArrayMethod {
+
+    public static void main(String[] args) {
+        String[] students = {"Ali", "Moh", "Hassan"};
+        int grades[][] = {{10, 10, 20}, {20, 100, 50}, {100, 100, 100}};
+        avstudent(students, grades);
+    }
+
+    static void avstudent(String[] names, int[][] grades) {
+        for (int i = 0; i < names.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < grades[i].length; j++) {
+                sum += grades[i][j];
+            }
+            System.out.println("Name: " + names[i] + " - Sum: " + sum + " - Average: " + (sum / names[i].length()));
+        }
+    }
+}
