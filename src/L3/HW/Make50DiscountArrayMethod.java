8479f92b931f0af161db6a6d5c1b94eaf00b94c5
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-02 12:36:01 +0300

    solve a question

---
+++ src/L3/HW/Make50DiscountArrayMethod.java
@@ -0,0 +1,40 @@
+package L3.HW;
+/*
+Q1: - Write java program to create array Price [4].
+1- input from keyboard any prices
+2- make discount 50% for each price in array then save results in new array discount[] then
+return new array and print result in main method.
+*/
+
+import java.util.Scanner;
+
+public class Make50DiscountArrayMethod {
+
+    public static void main(String[] args) {
+        int[] Price = new int[4];
+        enterArrayElements(Price);
+        printArrayElements(discount(Price));
+    }
+
+    static void enterArrayElements(int[] arr) {
+        System.out.print("Enter " + arr.length + " prices: ");
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    static int[] discount(int[] arr) {
+        int[] discount = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            discount[i] = (arr[i] * 50) / 100;
+        }
+        return discount;
+    }
+
+    static void printArrayElements(int[] arr) {
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
