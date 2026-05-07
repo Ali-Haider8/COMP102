a18fd486ab92ea236fd1ec69748ca68607a9de80
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-07 12:39:20 +0300

    solve a HW

---
+++ src/L3/HW/CheckPrimeMethod.java
@@ -0,0 +1,21 @@
+package L3.HW;
+
+public class CheckPrimeMethod {
+
+    public static void main(String[] args) {
+        int number = 7;
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number");
+        } else if (!isPrime(number)) {
+            System.out.println(number + " is not a prime number");
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
