a90f6342f75b8d5051e1c77ebbd7c223053e00a2
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-08 22:09:02 +0300

    Solve a HW

---
+++ src/L3/HW/SortArrayMethod.java
@@ -0,0 +1,34 @@
+package L3.HW;
+
+//2- Java Methods.pdf
+/*
+2- write code of java to sort x1[5], x2[4] using SortArr().
+*/
+
+public class SortArrayMethod {
+
+
+    static void sortArr(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] x1 = {5, 7, 2, 8, 6};
+        int[] x2 = {5, 3, 6, 3};
+
+        sortArr(x1);
+        for (int element : x1) System.out.print(element + " ");
+        System.out.println();
+        sortArr(x2);
+        for (int element : x2) System.out.print(element + " ");
+    }
+}
