ec300a3cf30bf7990e202e02df261d6b23906be4
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-08 23:59:50 +0300

    Solve a HW

---
+++ src/L3/FindSumAndMultiplicationMethod.java
@@ -0,0 +1,22 @@
+package L3;
+
+//3- Java Methods.pdf
+public class FindSumAndMultiplicationMethod {
+
+    static float AvNumbers(int a, int b) {
+        int Av = (a + b) / 2;
+        return Av;
+    }
+
+    static int multiNumbers(int num1, int num2) {
+        int mul = num1 * num2;
+        return mul;
+    }
+
+    public static void main(String[] args) {
+        float av = AvNumbers(5, 2);
+        int mul = multiNumbers(5, 5);
+        System.out.println("Average = " + av);
+        System.out.println("Multiplication = " + mul);
+    }
+}
