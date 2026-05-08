8ba46b40e10e078b8cd64aba6085a99fb8a63aaa
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-08 21:48:49 +0300

    solve HW from docs

---
+++ src/L3/HW/ThreeMethodToCalculateShapes.java
@@ -0,0 +1,30 @@
+package L3.HW;
+
+/*
+1- write java program to create three methods area of circle, circumference of a circle,
+and area of area of the triangle, pass parameter not return value.
+*/
+public class ThreeMethodToCalculateShapes {
+    static double piValue = 3.14;
+
+    static void circleArea(double radius) {
+        double circle_area = (radius * radius) * piValue;
+        System.out.println("Area of the circle = " + circle_area);
+    }
+
+    static void circleCircumference(double radius) {
+        int circle_circumference = (int) (2 * (piValue * radius));
+        System.out.println("Circumference of the circle = " + circle_circumference);
+    }
+
+    static void triangleArea(double base, double hieght) {
+        double triangle_area = (base * hieght) / 2;
+        System.out.println("Area of the triangle = " + triangle_area);
+    }
+
+    public static void main(String[] args) {
+        circleArea(5);
+        circleCircumference(5);
+        triangleArea(5, 5);
+    }
+}
