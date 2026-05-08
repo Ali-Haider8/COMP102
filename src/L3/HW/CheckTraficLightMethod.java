02a6fe6817e20c7fb0285228e14cf78174a2a423
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-09 01:15:16 +0300

    Solve a HW

---
+++ src/L3/HW/CheckTraficLightMethod.java
@@ -0,0 +1,31 @@
+package L3.HW;
+
+/*
+2- Write java program to create checkTraficlight(Light) , read the variable for
+keyboard inside method.
+
+Source: 4- H.W Methods using Java 2025.pdf
+*/
+
+import java.util.Scanner;
+
+public class CheckTraficLightMethod {
+
+    static void checkTraficLight(String color) {
+        String action = "";
+        if (color.equals("red")) action = "Stop";
+        else if (color.equals("yellow")) action = "Get Ready";
+        else if (color.equals("green")) action = "Go";
+        else System.out.println("Invalid Input");
+        System.out.println("Action: " + action);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the color of the traffic light");
+        System.out.println("red - " + "yellow - " + "green");
+        System.out.print("Current color is: ");
+        String color = sc.nextLine();
+        checkTraficLight(color);
+    }
+}
