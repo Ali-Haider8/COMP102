8aeb63a6f17d4a3926aebf63a2f2751d5b8276b7
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-08 20:34:28 +0300

    solve some examples from docs

---
+++ src/L3/CalculateMethod.java
@@ -0,0 +1,24 @@
+package L3;
+
+public class CalculateMethod {
+
+    static int num1 = 25;
+    static int num2 = 2;
+
+    // create a method
+    static void addNumbers() {
+        int sum = num1 + num2;
+        System.out.println("sum = " + sum);
+    }
+
+    static void multiplyNumbers(){
+        int mul = num1 * num2;
+        System.out.println("mul = "+mul);
+    }
+
+    public static void main(String[] args) {
+        // call method
+        addNumbers();
+        multiplyNumbers();
+    }
+}
---
+++ src/L3/MethodWithAParameter.java
@@ -0,0 +1,14 @@
+package L3;
+
+public class MethodWithAParameter {
+
+    static void myMethod(String fname) {
+        System.out.println(fname + " Refsnes");
+    }
+
+    public static void main(String[] args) {
+        myMethod("Lima");
+        myMethod("Jenny");
+        myMethod("Anja");
+    }
+}
