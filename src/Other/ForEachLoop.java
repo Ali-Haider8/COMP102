c6774282659b4f2e0d67792d98a016c2ba88f6c7
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-09 14:36:55 +0300

    for each loop class code refactoring and formatting

--- src/Other/ForEachLoop.java
+++ src/Other/ForEachLoop.java
@@ -8,13 +8,14 @@ public class ForEachLoop {
             System.out.print(element + " ");
         }
 
-        System.out.println();
+        System.out.println("\n");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
 
-        System.out.println();
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        System.out.println("\n");
         for (int[] row : matrix) {
             for (int element : row) {
                 System.out.print(element + " ");
