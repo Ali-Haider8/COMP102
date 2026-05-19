b4b521db0dba9e8a5439f6873d9e3b267b8895f6
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-19 15:55:46 +0300

    imporvment on code

--- src/L1/HW/FindAStringInArray.java
+++ src/L1/HW/FindAStringInArray.java
@@ -2,47 +2,35 @@ package L1.HW;
 
 import java.util.Scanner;
 
-// Q1: Write Java program to create array have 5 string items {Ali, Hassan ,
-//Mohaamed, Jaafar}
-//Then search for word Ali , if his name in array , print found if not print
-//not found.
+/*
+Q1: Write Java program to create array have 5 string items {Ali, Hassan ,
+Mohaamed, Jaafar}
+Source: 3- 1D Array Programs.pdf (page 2 of 2)
+*/
+
 public class FindAStringInArray {
 
-    static void main(String[] args) {
+    static Scanner sc = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
-        String[] names = new String[5];
+    static void enterArrayElements(String[] arr) {
+        System.out.println("Enter 5 names: ");
+        for (int i = 0; i < arr.length; i++) arr[i] = sc.nextLine();
+    }
 
-        System.out.println("Enter 5 names (Press enter after each name): ");
+    static boolean isNameExist(String[] arr, String name) {
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i].equalsIgnoreCase(name)) return true;
+        return false;
+    }
 
-        for (int i = 0; i < names.length; i++) {
-            names[i] = sc.nextLine();
-        }
+    public static void main(String[] args) {
+        String[] arr = new String[5];
+        enterArrayElements(arr);
 
-        System.out.println();
-        System.out.println("The elements of the array are: ");
+        System.out.println("Enter a name to find: ");
+        String name = sc.nextLine();
 
-        for (String name : names) {
-            System.out.print(name + " ");
-        }
-
-        System.out.println();
-        System.out.print("Find a name in array: ");
-        String targetName = sc.nextLine();
-        sc.close();
-
-        int flag = 0;
-        int i;
-        for (i = 0; i < names.length; i++) {
-            if (names[i].equals(targetName)) {
-                flag = i;
-            }
-        }
-
-        if (flag != 0) {
-            System.out.println("The name is found in position " + (flag + 1));
-        } else {
-            System.out.println("The name wasn't founded in the array");
-        }
+        if (isNameExist(arr, name)) System.out.println("Name is found");
+        else System.out.println("Name is not found");
     }
 }
