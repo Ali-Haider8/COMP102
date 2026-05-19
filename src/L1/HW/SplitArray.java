9d09e7c102554dce0a79460a9d2a943e0d6ded1b
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-19 18:15:58 +0300

    Improve SplitArray logic

--- src/L1/HW/SplitArray.java
+++ src/L1/HW/SplitArray.java
@@ -1,37 +1,32 @@
 package L1.HW;
 
-//  4. Create array (1, 2, 3, 4, 5, 6) then Split an Array into Two Halves:
-@SuppressWarnings("ManualArrayCopy")
+/*
+4. Create array (1, 2, 3, 4, 5, 6) then Split an Array into Two Halves:
+
+Source: 4- 1D Array H.W 2026.pdf (page 1 of 2)
+*/
+
 public class SplitArray {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {1, 2, 3, 4, 5, 6};
 
-        int[] A = new int[numbers.length / 2];
-        int[] B = new int[numbers.length / 2];
+        int[] arr1 = new int[arr.length / 2];
+        int[] arr2 = new int[arr.length / 2];
 
-        System.out.print("Original array: ");
-        for (int x : numbers) {
-            System.out.print(x + " ");
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = arr[i + arr1.length];
         }
 
-        for (int i = 0; i < (numbers.length / 2); i++) {
-            A[i] = numbers[i];
-        }
-
-        for (int i = 5; i < numbers.length; i++) {
-            B[i - 5] = numbers[i];
-        }
-
-        System.out.print("\nArray A: ");
-        for (int x : A) {
-            System.out.print(x + " ");
-        }
+        System.out.print("Array A: ");
+        for (int x : arr1) System.out.print(x + " ");
 
         System.out.print("\nArray B: ");
-        for (int x : B) {
-            System.out.print(x + " ");
-        }
+        for (int x : arr2) System.out.print(x + " ");
+
     }
 }
