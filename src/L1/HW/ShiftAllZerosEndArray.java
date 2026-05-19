2b8ee3184942cad7bb792288cded750c5a9ede4b
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-19 17:33:50 +0300

    Improve ShiftAllZerosEndArray logic

--- src/L1/HW/ShiftAllZerosEndArray.java
+++ src/L1/HW/ShiftAllZerosEndArray.java
@@ -1,34 +1,33 @@
 package L1.HW;
-//  2. Create integer array (0, 1, 0, 3, 12) then Shift All Zeroes to the End:
+
+/*
+2. Create integer array (0, 1, 0, 3, 12) then Shift All Zeroes to the End:
+
+Source: 4- 1D Array H.W 2026.pdf (page 1 of 2)
+*/
+
 public class ShiftAllZerosEndArray {
 
-    static void main(String[] args) {
-        int[] numbers = {0, 1, 0, 3, 12};
+    static void shiftZerosToEnd(int[] arr) {
         int index = 0;
-
-        System.out.print("The Array Elements Before Shifting: ");
-
-        for (int number : numbers) {
-            System.out.print(number + " ");
-        }
-
-        System.out.println();
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] != 0) {
-                numbers[index] = numbers[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
                 index++;
             }
         }
-
-        while (index < numbers.length) {
-            numbers[index] = 0;
+        while (index < arr.length) {
+            arr[index] = 0;
             index++;
         }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {0, 1, 0, 3, 12};
+        shiftZerosToEnd(numbers);
 
         System.out.print("The Array Elements After Shifting: ");
-        for (int number : numbers) {
-            System.out.print(number + " ");
-        }
+        for (int number : numbers) System.out.print(number + " ");
 
     }
 }
