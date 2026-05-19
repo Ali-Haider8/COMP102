da3795bba27a7cf0ff3ba169601c941b6a9889bf
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-19 17:16:33 +0300

    imporvment on code

--- src/L1/HW/FindACharArray.java
+++ src/L1/HW/FindACharArray.java
@@ -2,46 +2,43 @@ package L1.HW;
 
 import java.util.Scanner;
 
-//2- Rewrite second program and input char instead of string from keyboard
-// * second program:
-// Q2: Write java codes to create array with any five integer items { 5,4,2,1}
-//then sorting array in ascending order
+
+
+/*
+2- Rewrite second program and input char instead of string from keyboard
+
+* second program:
+Q1: Write Java program to create array have 5 string items {Ali, Hassan ,
+Mohaamed, Jaafar}
+Then search for word Ali , if his name in array , print found if not print
+not found.
+
+Source: 3- 1D Array Programs.pdf (page 2 of 2)
+*/
 
 public class FindACharArray {
 
-    static void main(String[] args) {
+    static boolean isCharInArray(char[] arr, char c) {
+        for (char ch : arr) if (ch == c) return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         char[] chars = new char[5];
 
         System.out.print("Enter 5 characters: ");
-
         for (int i = 0; i < chars.length; i++) {
             chars[i] = sc.next().charAt(0);
         }
 
-        System.out.print("Array items are: ");
-
-        for (char c : chars) {
-            System.out.print(c + " ");
-        }
-
-        System.out.println();
         System.out.print("Find a char in array: ");
         char a = sc.next().charAt(0);
         sc.close();
 
-        int flag = 0;
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == a) {
-                flag = i;
-            }
-        }
+        if (isCharInArray(chars, a)) System.out.println("Char founded in array");
+        else System.out.println("Char wasn't founded");
 
-        if (flag != 0) {
-            System.out.println("Char founded in position " + (flag + 1));
-        } else {
-            System.out.println("Char wasn't founded");
-        }
     }
 }
