c77290120963db787cef4dce9d3b85745e323cda
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-19 15:47:19 +0300

    edit code

--- src/L1/HW/SortArrayAlphabeticalOrder.java
+++ src/L1/HW/SortArrayAlphabeticalOrder.java
@@ -1,13 +1,23 @@
 package L1.HW;
 
-//Q> Write a Java program to sort names of an array in an alphabetical order.
+/*
+Q> Write a Java program to sort names of an array in an alphabetical order.
+
+Source: 2- One-dimensional Array Examples.pdf (page 4 of 4)
+*/
 
 public class SortArrayAlphabeticalOrder {
 
     public static void main(String[] args) {
 
         String[] arr = {"Zuhair", "Eman", "Ali", "Baqir"};
+        sortAlphabeticalOrder(arr);
+        for (String n : arr) System.out.print(n + " ");
 
+
+    }
+
+    static void sortAlphabeticalOrder(String[] arr) {
         String temp;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
@@ -18,10 +28,6 @@ public class SortArrayAlphabeticalOrder {
                 }
             }
         }
-
-        for (String n : arr) {
-            System.out.print(n + " ");
-        }
-
     }
+
 }
