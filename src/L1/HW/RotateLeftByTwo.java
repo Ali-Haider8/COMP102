be61ab8c330376346accf901e5b691d007c4ffdc
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-19 18:56:41 +0300

    make changes

--- src/L1/HW/RotateLeftByTwo.java
+++ src/L1/HW/RotateLeftByTwo.java
@@ -13,25 +13,17 @@ public class RotateLeftByTwo {
 
         int[] numbers = {10, 20, 30, 40, 50};
 
-        System.out.print("Array before left rotation: ");
-        for (int x : numbers) {
-            System.out.print(x + " ");
-        }
-
         for (int r = 0; r < 2; r++) {
             int first = numbers[0];
-
             for (int i = 0; i < numbers.length - 1; i++) {
                 numbers[i] = numbers[i + 1];
             }
-
             numbers[numbers.length - 1] = first;
         }
 
-        System.out.println();
+
         System.out.print("Array after left rotation: ");
-        for (int number : numbers) {
-            System.out.print(number + " ");
-        }
+        for (int number : numbers) System.out.print(number + " ");
+
     }
 }
