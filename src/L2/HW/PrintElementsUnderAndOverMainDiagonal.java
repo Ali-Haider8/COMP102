8bc4a2b5b5f712dffeb17b0253f8cd474e2a318b
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-22 16:10:54 +0300

    update code

--- src/L2/HW/PrintElementsUnderAndOverMainDiagonal.java
+++ src/L2/HW/PrintElementsUnderAndOverMainDiagonal.java
@@ -4,21 +4,19 @@ package L2.HW;
 Q4: write java program to print elements under and over the main diagonal in 4×4
 2D Array.
 
+Source: 2- Two-dimensional Array Examples.pdf (page 3 of 3)
 */
 public class PrintElementsUnderAndOverMainDiagonal {
 
     public static void main(String[] args) {
-        int[][] matrix = {
+        int[][] arr = {
                 {10, 11, 12, 13},
                 {14, 15, 16, 17},
                 {18, 19, 20, 21},
                 {22, 23, 24, 25}
         };
 
-        int row = matrix.length;
-        int col = matrix[0].length;
-
-        System.out.println("Array: ");
+        /*System.out.println("Array: ");
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 System.out.print(matrix[i][j] + "\t");
@@ -58,7 +56,18 @@ public class PrintElementsUnderAndOverMainDiagonal {
             }
             System.out.println();
         }
+        */
+
+
+        System.out.println("above the main diagonal");
+        for (int i = 0; i < arr.length; i++)
+            for (int j = 0; j < arr[i].length; j++)
+                if (j > i) System.out.print(arr[i][j] + " ");
+
+        System.out.println("\nunder the main diagonal");
+        for (int i = 0; i < arr.length; i++)
+            for (int j = 0; j < arr[i].length; j++)
+                if (i > j) System.out.print(arr[i][j] + " ");
+
     }
 }
-
-
