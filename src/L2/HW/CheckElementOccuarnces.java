7afdcfb003b2d5fe9e1abc47e4732ce28f0dcda7
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-22 15:10:38 +0300

    update code

--- src/L2/HW/CheckElementOccuarnces.java
+++ src/L2/HW/CheckElementOccuarnces.java
@@ -10,12 +10,12 @@ Source: 2- Two-dimensional Array Examples.pdf (page 3 of 3)
 
 public class CheckElementOccuarnces {
 
-    static int checkOccurrences(int[][] arr, int element) {
+    static void printOccurrences(int[][] arr, int element) {
         int count = 0;
         for (int i = 0; i < arr.length; i++)
             for (int j = 0; j < arr[i].length; j++)
                 if (arr[i][j] == element) count++;
-        return count;
+        System.out.println(element + " -> " + count);
     }
 
     public static void main(String[] args) {
@@ -23,6 +23,6 @@ public class CheckElementOccuarnces {
 
         for (int[] cols : arr)
             for (int n : cols)
-                System.out.println(n + " -> " + checkOccurrences(arr, n));
+                printOccurrences(arr, n);
     }
 }
