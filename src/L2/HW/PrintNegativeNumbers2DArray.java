2442010e4da53e8ad9bc7a03aef8f55dc58cd84a
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-22 14:43:44 +0300

    update

--- src/L2/HW/PrintNegativeNumbers2DArray.java
+++ src/L2/HW/PrintNegativeNumbers2DArray.java
@@ -10,9 +10,9 @@ public class PrintNegativeNumbers2DArray {
     public static void main(String[] args) {
         int[][] arr = {{1, -2, 3}, {-4, 5, 6}};
 
-        for (int i = 0; i < arr.length; i++)
-            for (int j = 0; j < arr[i].length; j++)
-                if (arr[i][j] < 0) System.out.print(arr[i][j] + " ");
+        for (int[] cols : arr)
+            for (int element : cols)
+                if (element < 0) System.out.print(element + " ");
 
     }
 }
