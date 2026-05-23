5332c9f83e1fb3b4fb21e63e739537cd82fbfb0a
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-23 17:20:44 +0300

    solve a question

--- src/L4/HW/FindDuplicateCharacters.java
+++ src/L4/HW/FindDuplicateCharacters.java
@@ -28,9 +28,22 @@ public class FindDuplicateCharacters {
         }
     }
 
+    static void findDuplicateCharacters(String text) {
+        char[] ch = text.toCharArray();
+        for (int i=0;i<ch.length;i++){
+            for (int j=i+1;j<ch.length;j++){
+                if (ch[i]==ch[j]){
+                    System.out.println(ch[i]+ " is duplicate character");
+                    break;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         String text = "Java is a programming language";
-        printDuplicateCharacters(text);
+//        printDuplicateCharacters(text);
+        findDuplicateCharacters("programming");
     }
 }
