7a4c76f0658def48f4a5626af6a5fd27e731a890
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-25 14:20:19 +0300

    make changes

--- src/L4/Others/UseSuitableMethod.java
+++ src/L4/Others/UseSuitableMethod.java
@@ -1,16 +1,34 @@
 package L4.Others;
 
 /*
-Source:  (page 1 of 1)
+ex1: use suitable method of string in this code
+
+Source: 1- JavaString2025.pdf (page 18 of 19)
 */
+
+import java.util.Scanner;
 
 public class UseSuitableMethod {
 
     public static void main(String[] args) {
 
-        String city = "Delhi";
-        if (city=="Meerut"){
-            System.out.println("city is meerut");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the city name: ");
+        String city = sc.nextLine();
+        sc.close();
+
+        switch (city) {
+            case "Meerut":
+                System.out.println("Meerut");
+                break;
+            case "Noida":
+                System.out.println("Noida");
+                break;
+            case "Agra":
+                System.out.println("Agra");
+                break;
+            default:
+                System.out.println("Delhi");
         }
 
     }
