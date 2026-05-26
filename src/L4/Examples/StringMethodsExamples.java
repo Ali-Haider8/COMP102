bf38ab46a27692b3080e792a334d3e6b43c8035d
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-27 00:02:50 +0300

    make changes

--- src/L4/Examples/StringMethodsExamples.java
+++ src/L4/Examples/StringMethodsExamples.java
@@ -4,12 +4,16 @@ package L4.Examples;
 Source: 1- JavaString2025.pdf (page 8 of 19)
 */
 
-public class CharAt {
 
-    public static void main(String[] args) {
+public class StringMethodsExamples {
+    static void charAt() {
         String name = "javatpoint";
         char ch = name.charAt(4); // returns the char value at the 4th index
         System.out.println(ch);
+    }
+
+    public static void main(String[] args) {
+
 
     }
 }
