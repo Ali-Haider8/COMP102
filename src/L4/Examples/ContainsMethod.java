09bfeb8a5f4bad7c24739a31d0df3a0a7ad5627b
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-28 16:19:40 +0300

    new code

---
+++ src/L4/Examples/ContainsMethod.java
@@ -0,0 +1,17 @@
+package L4.Examples;
+
+/*
+Source: 1- JavaString2025.pdf (page 11 of 19)
+*/
+
+public class ContainsMethod {
+
+    public static void main(String[] args) {
+
+        String name = "what do you know about me";
+        System.out.println(name.contains("me"));
+        System.out.println(name.contains("about"));
+        System.out.println(name.contains("hello"));
+
+    }
+}
