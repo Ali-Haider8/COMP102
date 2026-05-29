6df8bb3e649b7630e784eecf8ca437777aa93a94
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-29 17:59:41 +0300

    new code

--- src/L3/Examples/NamesArrayMethods.java
+++ src/L3/Examples/NamesArrayMethods.java
@@ -25,53 +25,37 @@ public class NamesArrayMethods {
             System.out.print("Enter 1 to find, 2 to sort, 3 to exit: ");
             choice = sc.nextInt();
             switch (choice) {
-                case 1:
-                    Find(Names, "Ali");
-                    break;
-                case 2:
-                    SortName(Names);
-                    break;
-                case 3:
-                    System.out.println("Exiting...");
-                    sc.close();
-                    break;
-                default:
-                    System.out.println("Invalid choice");
+                case 1: Find(Names, "Ali"); break;
+                case 2: SortName(Names); break;
+                case 3: System.out.println("Exiting..."); sc.close(); break;
+                default: System.out.println("Invalid choice");
             }
         } while (choice != 3);
     }
 
     static void Find(String[] Names, String key) {
         boolean found = false;
-        for (int i = 0; i < Names.length; i++) {
+        for (int i = 0; i < Names.length; i++)
             if (Names[i].equalsIgnoreCase(key)) {
                 System.out.println(key + " found at index " + i);
                 found = true;
                 break;
             }
-        }
-
-        if (!found) {
-            System.out.println(key + " not found");
-        }
+        if (!found) System.out.println(key + " not found");
     }
 
     static void SortName(String[] Names) {
         for (int i = 0; i < Names.length; i++) {
             String temp;
-            for (int j = 0; j < Names.length; j++) {
+            for (int j = 0; j < Names.length; j++)
                 if (Names[i].charAt(0) < Names[j].charAt(0)) {
                     temp = Names[i];
                     Names[i] = Names[j];
                     Names[j] = temp;
                 }
-            }
         }
-
         System.out.print("Sorted Names: ");
-        for (String name : Names) {
-            System.out.print(name + " ");
-        }
+        for (String name : Names) System.out.print(name + " ");
         System.out.println();
     }
 }
