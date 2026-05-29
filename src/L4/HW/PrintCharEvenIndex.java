bc75ca8422badc0a0d3f58dd8caeda307d5c5693
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-05-29 16:58:56 +0300

    new code

---
+++ src/L4/HW/PrintCharEvenIndex.java
@@ -0,0 +1,27 @@
+package L4.HW;
+
+/*
+Q1: Wrtie java program to read String from Keyboard then
+print char at every even index.
+
+Source: 3- JavaString HW.pdf (page 1 of 1)
+*/
+
+import java.util.Scanner;
+
+public class PrintCharEvenIndex {
+
+    static void printCharEvenIndex(String text) {
+        char[] ch = text.toCharArray();
+        for (int i = 0; i < ch.length; i++)
+            if (i % 2 == 0) System.out.println(ch[i] + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+        printCharEvenIndex(text);
+
+    }
+}
