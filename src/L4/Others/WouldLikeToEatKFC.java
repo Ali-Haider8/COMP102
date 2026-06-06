37c73526e93796ccff01bd69afbdc9ec9b5c7b31
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-06-06 14:39:09 +0300

    update a code

--- src/L4/Others/WouldLikeToEatKFC.java
+++ src/L4/Others/WouldLikeToEatKFC.java
@@ -11,16 +11,21 @@ Source: All-Questions-Second-Course.pdf (page 14 of 24)
 public class WouldLikeToEatKFC {
 
     static String[] split(String s1) {
-        String part1 = s1.substring(0, 16);
-        String part2 = s1.substring(16);
-        return new String[]{part1, part2};
+        String[] parts = new String[2];
+        parts[0] = s1.substring(0, 16);
+        parts[1] = s1.substring(16);
+//        String[] splitS = s1.split(" ", 2);
+        return parts;
     }
 
     public static void main(String[] args) {
         String S1 = "I would like to eat KFC";
+        String S2;
+
         String[] result = split(S1);
         S1 = result[0];
-        String S2 = result[1];
+        S2 = result[1];
+
         System.out.println("S1: " + S1);
         System.out.println("S2: " + S2);
     }
