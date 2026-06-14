8c5eef180fdceb073f2818c7b7b359265da4f6d7
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-06-14 23:45:00 +0300

    new code

---
+++ src/L1/Others/FindValueOfArrayAfterExceution.java
@@ -0,0 +1,22 @@
+package L1.Others;
+
+/*
+5. Find the value of A[1] after execution of the following program.
+
+int[] A = {0, 2, 4, 1, 3};
+for(int i = 0; i < A.length; i++){
+    A[i] = A[(A[i] + 3) % A.length];
+}
+
+a- 0    b- 1    c- 3
+
+
+Source: final-exams-csklawat.pdf (page 7 of 11)
+*/
+
+public class FindValueOfArrayAfterExceution {
+
+    public static void main(String[] args) {
+
+    }
+}
